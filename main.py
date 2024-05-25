908e1993741b9501c2365ae801c88af7b2587f9a
Timur Agafonov <electroidios@gmail.com>
2024-05-26 01:01:20 +0300

    try to build 14

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-import flet
-from flet import *
-
-
-
-def main(page: flet.Page) -> None:
-    # page.add(flet.Text("SHULTE123"))
-    page.title = "Shulte"
-    # page.theme_mode = 'dark'
-    # page.scroll = flet.ScrollMode.HIDDEN
-    page.vertical_alignment = flet.MainAxisAlignment.CENTER
+# import flet
+# from flet import *
+#
+#
+#
+# def main(page: flet.Page) -> None:
+#     # page.add(flet.Text("SHULTE123"))
+#     page.title = "Shulte"
+#     # page.theme_mode = 'dark'
+#     # page.scroll = flet.ScrollMode.HIDDEN
+#     page.vertical_alignment = flet.MainAxisAlignment.CENTER
 
     # page.horizontal_alignment = 'center'
     # page.update()
     # page.window_width = 500
     # page.window_height = 500
     # page.add(flet.Column())
-    # page.resizable = False
-
-    page.add(
-        # 1
-        flet.Row(
-            [
-                flet.IconButton(flet.icons.HOME),
+    # # page.resizable = False
+    #
+    # page.add(
+    #     # 1
+    #     flet.Row(
+    #         [
+    #             flet.IconButton(flet.icons.HOME),
                 # flet.Icon(flet.icons.SETTINGS),
                 # flet.FloatingActionButton(text="Add",
                 #                           bgcolor=flet.colors.BROWN_900,
@@ -31,9 +31,9 @@ def main(page: flet.Page) -> None:
                 # flet.FloatingActionButton(text="Add",
                 #                           bgcolor=flet.colors.BROWN_900,
                 #                           foreground_color=flet.colors.ORANGE),
-                flet.Text("SHULTE123")
-            ],
-            alignment=flet.MainAxisAlignment.CENTER,
+            #     flet.Text("SHULTE123")
+            # ],
+            # alignment=flet.MainAxisAlignment.CENTER,
     #     ),
     #     # 2
     # flet.Row(
@@ -102,8 +102,8 @@ def main(page: flet.Page) -> None:
     #             flet.Text("SHULTE123",text_align=flet.TextAlign.CENTER)
     #         ],
     #         alignment=flet.MainAxisAlignment.CENTER,
-        )
-    )
+    #     )
+    # )
 
 
 # if __name__ == "__main__":
@@ -111,12 +111,42 @@ def main(page: flet.Page) -> None:
 # flet.app(target=main)
 
 # if __name__ == "__main__":
-flet.app(main, view=flet.AppView.WEB_BROWSER)
+# flet.app(main, view=flet.AppView.WEB_BROWSER)
 
 
 
 
+import flet as ft
 
+
+def main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.theme_mode = ft.ThemeMode.DARK
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+
+ft.app(main, view=ft.AppView.WEB_BROWSER)
 
 
 
